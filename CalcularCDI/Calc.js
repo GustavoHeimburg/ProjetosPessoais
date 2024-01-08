16afdcd5c0e090f88436e3455f7435c2bb6225bf
GustavoHeimburg <gutogustavogu@gmail.com>
2024-01-07 23:37:06 -0300

    Calc cdi

---
+++ CalcularCDI/Calc.js
@@ -0,0 +1,25 @@
+function calcularInvestimento() {
+
+    const valorInicial = parseFloat(document.getElementById('valorInicial').value);
+    const taxaRendimento = parseFloat(document.getElementById('taxaRendimento').value) / 100;
+    const prazo = parseInt(document.getElementById('prazo').value);
+    const unidadeTempo = document.getElementById('unidadeTempo').value;
+
+
+    let rendimento = 0;
+
+    if (unidadeTempo === 'meses') {
+        rendimento = valorInicial * (1 + taxaRendimento / 12) ** prazo - valorInicial;
+    } else if (unidadeTempo === 'anos') {
+        rendimento = valorInicial * (1 + taxaRendimento) ** (prazo * 12) - valorInicial;
+    }
+
+    const resultadoSemInvestir = valorInicial;
+
+    const resultadoDiv = document.getElementById('resultado');
+    resultadoDiv.innerHTML = `
+        <p><strong>Resultado sem Investir:</strong> R$ ${resultadoSemInvestir.toFixed(2)}</p>
+        <p><strong>Rendimento Total:</strong> R$ ${rendimento.toFixed(2)}</p>
+        <p><strong>Valor Final:</strong> R$ ${(valorInicial + rendimento).toFixed(2)}</p>
+    `;
+}
